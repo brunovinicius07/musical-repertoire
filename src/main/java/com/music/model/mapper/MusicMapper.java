b993e649cc07bee6685d7c2d494cbf510f4eed14
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-08-06 20:04:37 -0300

    Created MusicMapper

---
+++ src/main/java/com/music/model/mapper/MusicMapper.java
@@ -0,0 +1,16 @@
+package com.music.model.mapper;
+
+import com.music.model.dto.request.MusicRequestDto;
+import com.music.model.dto.response.MusicResponseDto;
+import com.music.model.entity.Music;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MusicMapper {
+
+    @Mapping(target = "cdMusic", ignore = true)
+    Music toMusic(MusicRequestDto musicRequestDto);
+
+    MusicResponseDto toMusicResponseDto(Music music);
+}
