11b662890a5a01a348580405f96c8fe35af3d921
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-09-05 14:33:10 -0300

    Applied the test in the main class

--- src/test/java/com/music/MusicApplicationTests.java
+++ src/test/java/com/music/MusicApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MusicApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		MusicApplication.main(new String[] {});
 	}
 
 }
