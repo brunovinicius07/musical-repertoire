37b5e4964072da2cd61a8a68c944a72ce23d2877
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-10-18 16:12:42 -0300

    Created success and failure test for the getAllMusic method

--- src/main/java/com/music/model/mapper/GenderMapper.java
+++ src/main/java/com/music/model/mapper/GenderMapper.java
@@ -4,15 +4,11 @@ import com.music.model.dto.request.GenderRequestDto;
 import com.music.model.dto.response.GenderResponseDto;
 import com.music.model.entity.Gender;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GenderMapper {
 
     Gender toGender(GenderRequestDto genderRequestDto);
 
-
     GenderResponseDto toGenderResponseDto(Gender gender);
-
-
 }
--- src/test/java/com/music/services/impl/MusicServiceImplTest.java
+++ src/test/java/com/music/services/impl/MusicServiceImplTest.java
@@ -2,6 +2,7 @@ package com.music.services.impl;
 
 import com.music.exception.AlertException;
 import com.music.model.dto.request.MusicRequestDto;
+import com.music.model.dto.response.GenderResponseDto;
 import com.music.model.dto.response.MusicResponseDto;
 import com.music.model.entity.Gender;
 import com.music.model.entity.Music;
@@ -70,7 +71,42 @@ class MusicServiceImplTest {
     }
 
     @Test
-    void getAllMusic() {
+    void getAllMusic_success() {
+        when(musicRepository.findAll()).thenReturn(List.of(music));
+        when(musicMapper.toMusicResponseDto(any())).thenReturn(musicResponseDto);
+
+        List<MusicResponseDto> response = musicService.getAllMusic();
+
+        verify(musicRepository, times(1)).findAll();
+        verify(musicMapper, times(1)).toMusicResponseDto(any());
+        verifyNoMoreInteractions(musicRepository, musicMapper);
+
+        assertNotNull(response);
+        assertEquals(CD_MUSIC, response.get(0).getCdMusic());
+        assertEquals(NM_MUSIC, response.get(0).getNmMusic());
+        assertEquals(SINGER, response.get(0).getSinger());
+    }
+
+    @Test
+    void getAllMusic_failure() {
+        when(musicRepository.findAll()).thenReturn(List.of());
+        when(musicMapper.toMusicResponseDto(any())).thenReturn(musicResponseDto);
+
+
+        AlertException exception = assertThrows(AlertException.class, () -> {
+            musicService.getAllMusic();
+        });
+
+        verify(musicRepository, times(1)).findAll();
+        verify(musicMapper, never()).toMusicResponseDto(any());
+        verify(musicRepository).findAll();
+        verify(musicMapper, never()).toMusicResponseDto(any());
+
+        assertNotNull(exception.getMessage());
+        assertEquals("Nenhuma música encontrada!", exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
+        assertEquals("warn", exception.getErrorCode());
+        assertThrows(AlertException.class, () -> musicService.getAllMusic());
     }
 
     @Test
